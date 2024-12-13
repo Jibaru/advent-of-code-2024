7207e73a029d5eba27f738ae06ebe96f67ce0aaf
Jibaru <ignacioruedaboada@hotmail.com>
2024-12-13 11:25:35 -0500

    feat: update main

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	day10 "github.com/jibaru/advent-of-code-2024/day_10"
 	day11 "github.com/jibaru/advent-of-code-2024/day_11"
 	day12 "github.com/jibaru/advent-of-code-2024/day_12"
+	day13 "github.com/jibaru/advent-of-code-2024/day_13"
 	day2 "github.com/jibaru/advent-of-code-2024/day_2"
 	day3 "github.com/jibaru/advent-of-code-2024/day_3"
 	day4 "github.com/jibaru/advent-of-code-2024/day_4"
@@ -55,6 +56,8 @@ func main() {
 		answer, err = day11.Solve(*part, *isTest)
 	case 12:
 		answer, err = day12.Solve(*part, *isTest)
+	case 13:
+		answer, err = day13.Solve(*part, *isTest)
 	default:
 		err = fmt.Errorf("day not allowed")
 	}
