42f578512fef89945b1d182de1e2f8a7bae5af3e
Jibaru <ignacioruedaboada@hotmail.com>
2024-12-22 20:58:48 -0500

    Day 21: Keypad Conundrum

---
+++ day_21/solve.go
@@ -0,0 +1,35 @@
+package day21
+
+import (
+	"fmt"
+	"os"
+)
+
+func Solve(part int, isTest bool) (any, error) {
+	f := "day_21/input.txt"
+	if isTest {
+		f = "day_21/input-test.txt"
+	}
+
+	body, err := os.ReadFile(f)
+	if err != nil {
+		return nil, err
+	}
+
+	switch part {
+	case 1:
+		return partOne(string(body))
+	case 2:
+		return partTwo(string(body))
+	}
+
+	return nil, fmt.Errorf("part should be only 1 or 2")
+}
+
+func partOne(_ string) (any, error) {
+	return "part 1 ok", nil
+}
+
+func partTwo(_ string) (any, error) {
+	return "part 2 ok", nil
+}
